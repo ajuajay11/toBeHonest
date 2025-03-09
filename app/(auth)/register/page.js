a442c4931817d06f6c0da453c77ca616b799491e
Ajay k <83286361+ajuajay11@users.noreply.github.com>
2025-03-09 18:06:45 +0400

    updated

--- app/(auth)/register/page.js
+++ app/(auth)/register/page.js
@@ -1,26 +1,38 @@
-export default async function Page() {
-  const res = await fetch('https://to-be-honest.vercel.app/api/chroniclesOfEveryOne/')
+// export default async function Page() {
+//   const res = await fetch('http://localhost:3000/api/chroniclesOfEveryOne/')
 
-  // Check for successful response
-  if (!res.ok) {
-    console.error('Failed to fetch:', res.statusText)
-    return <div>Error fetching data</div>
-  }
+//   // Check for successful response
+//   if (!res.ok) {
+//     console.error('Failed to fetch:', res.statusText)
+//     return <div>Error fetching data</div>
+//   }
 
-  // Try parsing the response to JSON
-  let posts = {}
-  try {
-    posts = await res.json()
-  } catch (error) {
-    console.error('Error parsing JSON:', error)
-    return <div>Error parsing data</div>
-  }
+//   // Try parsing the response to JSON
+//   let posts = {}
+//   try {
+//     posts = await res.json()
+//   } catch (error) {
+//     console.error('Error parsing JSON:', error)
+//     return <div>Error parsing data</div>
+//   }
 
+//   return (
+//     <ul>
+//       {posts.darkTruths?.map((post) => (
+//         <li key={post.yourStoryTitle}>{post.chroniclesOfYou}</li>
+//       ))}
+//     </ul>
+//   )
+// }
+
+ 
+
+export default function page() {
   return (
-    <ul>
-      {posts.darkTruths?.map((post) => (
-        <li key={post.yourStoryTitle}>{post.chroniclesOfYou}</li>
-      ))}
-    </ul>
+    <>
+    
+    hello</>
   )
 }
+
+ 

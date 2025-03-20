0584e10acbd7dea960802b22d1c944607631414b
Ajay k <83286361+ajuajay11@users.noreply.github.com>
2025-03-20 09:35:32 +0400

    aaded

--- app/(auth)/loginAuth/page.js
+++ app/(auth)/loginAuth/page.js
@@ -1,5 +1,5 @@
-'use client'
- import axios from "axios";
+'use client';
+import axios from "axios";
 import { useState } from "react";
 import Cookies from 'js-cookie';
 
@@ -8,35 +8,54 @@ export default function Register() {
     email: "",
     password: ""
   });
-  const handleSubmit= async (e)=> {
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const response = await axios.post(
-         `https://to-be-honest.vercel.app/api/user/login/`,
-         loginData,
-         {
-           headers: {
-             "Content-Type": "application/json",
-             "Host": "to-be-honest.vercel.app",
-           },
-         }
-       );
+        `/api/user/login/`,
+        loginData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
       console.log(response);
-      if (response) {
+      if (response.data.user.token) {
         Cookies.set('token', response.data.user.token, { expires: 12 });
         Cookies.set('isAuthenticated', true, { expires: 12 });
-      } 
+      }
     } catch (error) {
       console.log(error);
     }
-  }
+  };
+
   return (
     <>
-      <form onClick={handleSubmit}>
-        <input className="form-control" value={loginData.email} onChange={(e)=>setLoginData({...loginData, email:e.target.value})} placeholder="enter your email" type="email" required id="email" />
-        <input className="form-control" value={loginData.password} onChange={(e)=>setLoginData({...loginData, password:e.target.value})} placeholder="enter your password" type="email" required id="password" />
-        <button className="button btn-primary mt-3">Submit</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="form-control"
+          value={loginData.email}
+          onChange={(e) => setLoginData({ ...loginData, email: e.target.value })}
+          placeholder="Enter your email"
+          type="email"
+          required
+          id="email"
+        />
+        <input
+          className="form-control"
+          value={loginData.password}
+          onChange={(e) => setLoginData({ ...loginData, password: e.target.value })}
+          placeholder="Enter your password"
+          type="password"
+          required
+          id="password"
+        />
+        <button type="submit" className="button btn-primary mt-3">
+          Submit
+        </button>
       </form>
     </>
-  )
+  );
 }
